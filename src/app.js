6ed03599b95a6b9f079cff7025acac5e5c3094f5
Sarah <hayes.sc@gmail.com>
2017-11-02 07:25:32 -0400

    More styling and linking to public path in app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@
 
 const express = require('express');
 const config = require('./config');
+const path = require('path');
 
 const app = express();
+
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
 
 app.use(function(req, res, next) {
   res.end("Hello World!");
